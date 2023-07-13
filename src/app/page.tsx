1227d77e2632333e791ca339505b26dc92006794
HARIHARAN <hariharan.k@wealthmindcreation.com>
2023-07-13 11:16:43 +0530

    before & after labels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Human from "public/human.jpg";
 
 export default function Home(): ReactElement {
   return (
-    <section className="w-full h-max pb-40 relative flex justify-center">
-      <div className="w-[1100px] h-screen mx-auto flex flex-col justify-end items-center relative overflow-x-hidden overflow-y-clip pb-40">
-        <div className="h-[800px] w-[1100px] absolute top-0 bottom-0 my-auto left-1/2 -translate-x-1/2">
-          <div className="w-[800px] h-full mx-auto relative flex justify-center items-center overflow-y-clip  -translate-x-1/2">
+    <section className="w-full h-max relative flex justify-center">
+      <div className="w-[1300px] h-[900px] mx-auto flex flex-col justify-start items-center relative overflow-x-hidden overflow-y-clip">
+        <div className="h-[800px] w-[1100px] absolute top-0 m-auto left-1/2 -translate-x-1/2">
+          <div className="w-[800px]  h-full mx-auto relative flex justify-center items-center overflow-y-clip  -translate-x-1/2">
             <div className="animate-spin_anticlockwise w-full h-full m-auto border-gray-300 rounded-full border-2 border-dashed absolute -bottom-28 left-[50%] right-0 mx-auto"></div>
           </div>
           <div
@@ -47,12 +47,11 @@ export default function Home(): ReactElement {
             <i className="fa-solid text-gray-400 fa-angle-down"></i>
           </div>
           <div
-            key={`arrow_1`}
+            key={`arrow_7`}
             className="w-6 h-6 -rotate-90 rounded-full absolute -bottom-[13px] right-36 flex justify-center items-center bg-white z-40"
           >
             <i className="fa-solid text-gray-400 fa-angle-down"></i>
           </div>
-
           <div className="custom-border p-[1px] animate-border_dance w-full border-none flex-shrink-0"></div>
           <div className="w-[550px] h-1 bg-white absolute -bottom-1 left-0 right-0 mx-auto "></div>
           <div className="w-full h-max">
@@ -109,7 +108,7 @@ export default function Home(): ReactElement {
                       style={{
                         animationDelay: `${delay}s`,
                       }}
-                      className={`animate-highInterestRate w-20 h-max py-1 px-3 bg-teal-100 text-teal-700 rounded-lg text-sm absolute -bottom-10 -left-16 text-center`}
+                      className={`animate-highInterestRate w-20 h-max py-1 px-3 bg-teal-100 text-teal-700 rounded-lg text-sm absolute -bottom-8 -left-20 text-center`}
                     >
                       High Interest
                       <div
@@ -205,6 +204,51 @@ export default function Home(): ReactElement {
               );
             })}
           </div>
+
+          {/* Main content */}
+          <div className="w-1/2 h-max absolute top-0 bottom-0 left-0 right-0 m-auto space-y-[7%] bg-red-30 pt-[15%]">
+            <div className="w-full text-center space-y-[5%]">
+              <h1 className="text-4xl sm:text-5xl font-black text-gray-700 text-center md:text-6xl lg:text-6xl lg:w-[80%] mx-auto ">
+                Put your content here!
+                {/* <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-[#7EE687]">
+                  Your Dream{" "}
+                </span>
+                Home <br />
+                Our Expert{" "}
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-[#7EE687]">
+                  Guidance{" "}
+                </span> */}
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis consequatur excepturi aliquid qui maxime neque minus
+                hic ullam soluta aperiam illum nesciunt ipsa fuga doloribus
+                quis, sunt aut at odio consequuntur odit dolore! Culpa enim
+              </p>
+            </div>
+            <div className="w-full flex justify-center items-center space-x-3">
+              <button
+                className={`bg-gray-800 text-white rounded-md py-3 w-72 font-medium text-lg lg:w-60 lg:text-xl`}
+              >
+                Get started for free
+                <i className="fa-solid fa-angle-right ml-2"></i>
+              </button>
+              <button
+                className={` text-gray-600 border border-[#8c949e] rounded-md py-3 w-72 font-medium text-lg lg:w-60 lg:text-xl hover:border-[#8c949e]`}
+              >
+                Plans & Pricing
+                <i className="fa-solid fa-angle-right ml-2"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="px-3 bg-gray-50 py-1 w-max h-max absolute bottom-[9.5%] left-2 rounded-full text-gray-700 border border-gray-400 text-sm">
+          Before
+          <i className="fa-solid fa-forward ml-2 text-gray-500" />
+        </div>
+        <div className="px-3 bg-gray-50 py-1 w-max h-max absolute bottom-[9.5%] right-5 rounded-full text-gray-700 border border-gray-400 text-sm">
+          <i className="fa-solid fa-backward mr-2 text-gray-500" />
+          After
         </div>
       </div>
     </section>
